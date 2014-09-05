ea5e390d912d3889d0f20dc6ab68f00ca082082f
Zach Wentz <zkwentz@gmail.com>
2014-09-05 05:18:01 +0000

    added cli option to node module

---
+++ bin/require-cache.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+'use strict';
+var nopt    = require('nopt');
+var chalk   = require('chalk');
+var path    = require('path');
+var project = require('../lib');
+var pkg     = require('../package.json');
+
+
+
+var opts = nopt({
+    help: Boolean,
+    version: Boolean,
+    config: path,
+    assets: path,
+    'base-url': path,
+    baseUrl: path //alias
+}, {
+    h: '--help',
+    v: '--version',
+    c: '--config',
+    a: '--assets',
+    b: '--base-url',
+});
+
+if (opts['base-url']) {
+    opts.baseUrl = opts['base-url'];
+}
+
+function init() {
+    project(opts);
+}
+
+
+function help() {
+    //to be continued...
+}
+
+function pre() {
+    if (opts.version) {
+        return console.log(pkg.version);
+    }
+    
+    if (opts.help) {
+        return console.log(help());
+    }
+    
+    init();
+}
+
+pre();
